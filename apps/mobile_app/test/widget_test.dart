63628c020a12ae99fd4695305b09199f9113bcb6
Pannenama <preciouslabarete@gmail.com>
2026-02-03 21:29:11 +0800

    added widget_test.dart

--- apps/mobile_app/test/widget_test.dart
+++ apps/mobile_app/test/widget_test.dart
@@ -11,20 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:alaga/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Alaga app builds', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const AlagaApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Simple smoke test: ensure a MaterialApp is present.
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
